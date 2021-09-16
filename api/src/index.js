142d03782c4715eef0cae7dcbf45a1ba7a887e81
nicocchina <ra41105586898@acaonsfatima.org.br>
2021-09-16 02:02:21 -0300

    index.js

--- api/src/index.js
+++ api/src/index.js
@@ -8,22 +8,76 @@ app.use(express.json());
 
 app.get('/produto', async (req, resp) => {
     try {
+        let produtos = await db.tb_produto.findAll({
+                             order: [[ 'id_matricula', 'desc']]
+                        });
 
     } catch (e) {
-        
+        resp.send({erro: e.toString()});
     }
 })
 
 app.post('/produto', async (req, resp) => {
-    
+    try {
+        let { produto, categoria, preco_de, preco_por, avaliacao, descproduto, estoque, imgproduto, ativo, inclusao } = req.body;
+
+        let r = await db.tb_produto.create({
+            nm_produto: produto,
+            ds_categoria: categoria,
+            vl_preco_de: preco_de,
+            vl_preco_por: preco_por,
+            vl_avaliacao: avaliacao,
+            ds_produto: descproduto,
+            qtd_estoque: estoque,
+            img_produto: imgproduto,
+            bt_ativo: ativo,
+            dt_inclusao: inclusao
+        })
+        resp.send(r);
+    } catch (e) {
+        resp.send({erro: e.toString()});
+    }
 })
 
 app.put('/produto/:id', async (req, resp) => {
-    
+    try {   
+        let { produto, categoria, preco_de, preco_por, avaliacao, descproduto, estoque, imgproduto, ativo, inclusao } = req.body;
+        let { id } = req.params;
+        
+        let r = await db.tb_produto.update(
+            {
+            nm_produto: produto,
+            ds_categoria: categoria,
+            vl_preco_de: preco_de,
+            vl_preco_por: preco_por,
+            vl_avaliacao: avaliacao,
+            ds_produto: descproduto,
+            qtd_estoque: estoque,
+            img_produto: imgproduto,
+            bt_ativo: ativo,
+            dt_inclusao: inclusao
+            },
+            {
+                where: {id_produto: id }
+            }
+        )
+        resp.sendStatus(200);
+    } catch (e) {
+        resp.send({erro: e.toString()});
+    }
 })
 
 app.delete('/produto/:id', async (req, resp) => {
-    
+    try {
+        let { id } = req.params;
+
+        let r  = await db.tb_produto.destroy({
+            where: { id_produto: id }
+        })
+        resp.sendStatus(200);
+    } catch (e) {
+        resp.send({erro: e.toString()});
+    }
 })
 
 
